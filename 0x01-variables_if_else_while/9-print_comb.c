eb2c2e74acaf3cd4a5f191a1b7d9925c1df5c691
yemu-hub <yemiwed@gmail.com>
2022-12-09 15:40:15 -0800

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-entry prgram.
+ * Return: success 0.
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		putchar((number % 10) + '0');
+		if (number == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
