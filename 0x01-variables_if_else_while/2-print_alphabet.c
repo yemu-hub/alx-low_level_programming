3a872ac87ea1af94ee2f51bc2134d5bcb206ce4b
yemu-hub <yemiwed@gmail.com>
2022-12-09 13:59:20 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-program entry.
+ * Return: success 0.
+ */
+int main(void)
+{
+	char let;
+	for (for let = 'a'; let <= 'z' let++)
+		putchar(let);
+	putchar('\n');
+
+	return(0);
+}
