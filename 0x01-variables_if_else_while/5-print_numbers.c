513b146b748bdcb30ecc6842d742c9b7347822b5
yemu-hub <yemiwed@gmail.com>
2022-12-09 14:43:18 -0800

    print alphbet 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * maint -program entry.
+ * Return: success 0.
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		printf("%d", number);
+		number++;
+	}
+	printf("\n");
+	return (0);
+}
