e93d75f9313cc0149c752407717d79b686754976
yemu-hub <yemiwed@gmail.com>
2022-12-15 05:00:22 -0800

    My header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 
 int _isupper(int c);
 int _putchar(char ch);
@@ -10,4 +12,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n); 
+void print_number(int n);
+
+#endif /* MAIN_H */
