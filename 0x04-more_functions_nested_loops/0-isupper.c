b9a359ef43354a95b8b92ba00c94a35f55553d6d
yemu-hub <yemiwed@gmail.com>
2022-12-15 04:18:15 -0800

    ispper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include <main.h>
 /**
- * _isppuer - checks whether a charctor is uppercase.
+ * _isupper - checks whether a charctor is uppercase.
  * @c: input parameter.
  * Return: 1 if charcter is uppercase, 0 if not.
  */
