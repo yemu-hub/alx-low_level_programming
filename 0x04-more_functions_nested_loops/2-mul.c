8fccb3a9f39fc366d4bfbef851133c6951027b8c
yemu-hub <yemiwed@gmail.com>
2022-12-15 12:14:14 -0800

    product

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,10 +3,10 @@
  * mul - function that multiplies two integers.
  * @a: first input parameter.
  * @b: second input parameter.
- * Return: resopond product.
+ * Return: respond product.
  */
 
 int mul(int a, int b)
 {
-	return(a*b);
+	return (a*b);
 }
