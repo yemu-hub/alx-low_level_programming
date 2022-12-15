ae5692fdc9114f45d9ce3d6485075aa02c98b36f
yemu-hub <yemiwed@gmail.com>
2022-12-15 14:45:52 -0800

    print number again

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,10 +11,10 @@ void print_number(int n)
 	if ( n < 0)
 	{
 		_putchar('_');
-		num =-num;
+		num =- num;
 	}
 
-	if ((num / 10)> 0)
+	if ((num / 10) > 0)
 		print_number(num / 10);
 	_putchar((num % 10) + '0');
 }
